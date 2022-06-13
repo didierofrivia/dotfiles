34ec72a341931687dd3d33cd48a07b69d39dc758
dd di cesare <didi@posteo.net>
2022-06-13 21:22:57 +0200

    [script] Removing everything Ruby

--- install.sh
+++ install.sh
@@ -61,27 +61,6 @@ brew bundle
 fancy_echo "Setting Zsh as default..."
 chsh -s $(which zsh)
 
-fancy_echo "Configuring Ruby ..."
-find_latest_ruby() {
-  rbenv install -l | grep -v - | tail -1 | sed -e 's/^ *//'
-}
-
-ruby_version="$(find_latest_ruby)"
-# shellcheck disable=SC2016
-append_to_zshrc 'eval "$(rbenv init - --no-rehash)"' 1
-eval "$(rbenv init -)"
-
-if ! rbenv versions | grep -Fq "$ruby_version"; then
-  RUBY_CONFIGURE_OPTS=--with-openssl-dir=/usr/local/opt/openssl rbenv install -s "$ruby_version"
-fi
-
-rbenv global "$ruby_version"
-rbenv shell "$ruby_version"
-gem update --system
-gem_install_or_update 'bundler'
-number_of_cores=$(sysctl -n hw.ncpu)
-bundle config --global jobs $((number_of_cores - 1))
-
 # Set macOS preferences, originally from https://github.com/mathiasbynens/dotfiles
 fancy_echo "Set macOS preferences ..."
 source .macos
